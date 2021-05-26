b85593a2eb89937a0eb4a20620f33b13949b3c49
Tryan Gilang <tryan.gilang@gmail.com>
2021-05-26 21:57:22 +0700

    Refactor code & add config file

--- scan.rb
+++ scan.rb
@@ -2,7 +2,7 @@
 
 require "digest"
 
-def scan(path)
+def list_files(path)
     filepaths = []
     filenames = Dir.entries(path)
 
@@ -14,7 +14,7 @@ def scan(path)
         filepath = "#{path}/#{filename}"
 
         if File.directory?(filepath)
-            arr = scan(filepath)
+            arr = list_files(filepath)
             filepaths.append(*arr)
 
             next
@@ -26,47 +26,51 @@ def scan(path)
     return filepaths
 end
 
-file_index = {}
-file_index_c = {}
+def scan(path)
+    file_index = {}
+    file_index_c = {}
 
-filenames = scan("./DropsuiteTest")
+    filenames = list_files(path)
 
-# hash each files, put in on file_index
-filenames.each { |filepath|
-    filehash = (Digest::SHA256.file filepath).to_s
+    # hash each files, put in on file_index
+    filenames.each { |filepath|
+        filehash = (Digest::SHA256.file filepath).to_s
 
-    if !file_index.key?(filehash)
-        file_index[filehash] = []
-    end
-
-    if !file_index_c.key?(filehash)
-        file_index_c[filehash] = 0
-    end
-
-    file_index[filehash] << filepath
-    file_index_c[filehash] += 1
-}
-
-keys = file_index_c.sort_by {|key, val| -val}.map {|item| item[0]}
-max = file_index_c.values.max
-
-if max > 1 && keys.length > 0
-    keys.each_with_index { |key, index|
-        if file_index_c[keys[index]] < max
-            next
+        if !file_index.key?(filehash)
+            file_index[filehash] = []
         end
 
-        filepath = file_index[keys[index]][0]
-
-        if !File.exists?(filepath)
-            next
+        if !file_index_c.key?(filehash)
+            file_index_c[filehash] = 0
         end
 
-        file = File.open(filepath)
-        content = file.read
-
-        puts "#{content} #{file_index_c[keys[index]]}"
+        file_index[filehash] << filepath
+        file_index_c[filehash] += 1
     }
-else
-    puts "No file duplication"
+
+    keys = file_index_c.sort_by {|key, val| -val}.map {|item| item[0]}
+    max = file_index_c.values.max
+
+    if max > 1 && keys.length > 0
+        keys.each_with_index { |key, index|
+            if file_index_c[keys[index]] < max
+                next
+            end
+
+            filepath = file_index[keys[index]][0]
+
+            if !File.exists?(filepath)
+                next
+            end
+
+            file = File.open(filepath)
+            content = file.read
+
+            puts "#{content} #{file_index_c[keys[index]]}"
+        }
+    else
+        puts "No file duplication"
+    end
 end
+
+scan("./DropsuiteTest")
